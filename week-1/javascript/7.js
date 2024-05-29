0d2ddbcdf370b57f204befb52d0196cb3998edd5
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-29 16:59:24 +0800

    add integer checker that catches a custom error if the input is not an integer

--- week-1/javascript/7.js
+++ week-1/javascript/7.js
@@ -3,3 +3,15 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+const integerCheck = (num) => {
+    try {
+        if(typeof num !== 'number'){
+            throw ("The Number Inputted is not an Integer")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+integerCheck('2')
