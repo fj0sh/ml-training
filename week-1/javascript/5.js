a4d075a60d2f15878c16b7d5c7f4582a44ad5546
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-29 16:33:36 +0800

    [JS-5] add function that converts celsius to fahrenheit

--- week-1/javascript/5.js
+++ week-1/javascript/5.js
@@ -4,3 +4,10 @@
  * @param {number} celsius The temperature in Celsius.
  * @return {number} The temperature in Fahrenheit.
  */
+
+const celsiusConverter = (celsius) => {
+    const fahrenheit = (9/5 * celsius) + 32
+    console.log(`${celsius}°C = ${fahrenheit}°F`);
+}
+
+celsiusConverter(60)
