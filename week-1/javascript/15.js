ddf6577eaafef7eefecb73f5a0cb03482c4dab49
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-30 09:59:21 +0800

    [JS-15 add numberIterator function that iterates through a number and returns whether its od or even

--- week-1/javascript/15.js
+++ week-1/javascript/15.js
@@ -2,3 +2,17 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+
+function numberIterator(n){
+    for(x = 1; x <= n; x++){
+        if (x % 2 == 0) {
+            console.log("even");
+        }else{
+            console.log("odd");
+        }
+    }
+}
+
+console.log(numberIterator(20));
+
