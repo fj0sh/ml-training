87245fd1e4a21ecd2c6aec264209e8e57c433dff
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-30 14:45:43 +0800

    [JS-13] add uppercaser function that changes the first letter of every word to uppercase

--- week-1/javascript/13.js
+++ week-1/javascript/13.js
@@ -6,3 +6,16 @@
  * Example string: "The quick brown fox jumps over the lazy dog"
  * Expected output: "The Quick Brown Fox Jumps Over The Lazy Dog"
  */
+
+function upperCaser(str) {
+    let words = str.split(" ");
+    for (let x = 0; x < words.length; x++) {
+      words[x] = words[x][0].toUpperCase() + words[x].substring(1);
+    }
+  
+    return words.join(" ");
+  }
+  
+  console.log(upperCaser("The quick brown fox jumps over the lazy dog"));
+
+  
