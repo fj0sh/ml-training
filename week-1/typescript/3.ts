f760fa3c2ddfb9f3bfea8afa8a0b6fd1f11781de
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 10:33:46 +0800

    [JS-3] add a fizzbuzz fn that returns the values fizz, buzz, and fizzbuzz in a given conditions

--- week-1/typescript/3.ts
+++ week-1/typescript/3.ts
@@ -5,3 +5,19 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+const fizzBuzz = (n: number) => {
+  for (let x = 1; x <= n; x++) {
+    if (x % 15 == 0) {
+      console.log("fizzbuzz");
+    } else if (x % 3 == 0) {
+      console.log("fizz");
+    } else if (x % 5 == 0) {
+      console.log("buzz");
+    } else {
+      console.log(x);
+    }
+  }
+};
+
+fizzBuzz(20);
