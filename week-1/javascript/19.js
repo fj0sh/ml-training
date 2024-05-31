484571b4f3e88c8526ba09121e91304fd5a7508e
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 13:49:29 +0800

    [JS-19] Add arrMerger function  which merges two arrays and remove its duplicates

--- week-1/javascript/19.js
+++ week-1/javascript/19.js
@@ -9,3 +9,13 @@
  * @param {number[]} arr2
  * @returns {number[]}
  */
+
+const arrMerger = (arr1, arr2) => {
+
+   let newArr = arr1.concat(arr2);
+   let mergedArr = [...new Set(newArr)]
+
+   return mergedArr
+}
+
+console.log(arrMerger([1,2,3,4,5],[1,2,3,4,5,6]));
