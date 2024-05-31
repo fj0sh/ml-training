0115dc1d665a63eb6273938e429d3ec7465167c0
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 16:38:56 +0800

    [TS-12] add arrHandler function that throws an error if the array is empty

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,17 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function arrHandler(...arr: number[]) {
+  try {
+    if (arr.length === 0) {
+      //   throw "Array is Empty";
+      throw new Error("Array is Empty");
+    }
+    return arr.length;
+  } catch (error) {
+    return error;
+  }
+}
+
+console.log(arrHandler());
