24c8659b5c76510eb886a3e17ead3bec6a2dfc10
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 10:30:43 +0800

    [TS-2] add a sum fn that returns the sum of the array of number

--- week-1/typescript/2.ts
+++ week-1/typescript/2.ts
@@ -3,3 +3,12 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+function Sum(...arr: number[]): number {
+  let total: number = 0;
+  for (const n of arr) {
+    total += n;
+  }
+  return total;
+}
+console.log(Sum(5, 20));
