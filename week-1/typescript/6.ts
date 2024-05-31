63e34740a7b2209d6747b766882e8124798b6203
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 15:55:30 +0800

    [TS-6] add a fahrenheitConvert function that converts fahrenheit to celsius

--- week-1/typescript/6.ts
+++ week-1/typescript/6.ts
@@ -4,3 +4,11 @@
  * @param {number} fahrenheit The temperature in Fahrenheit.
  * @return {number} The temperature in Celsius.
  */
+
+const fahrenheitConvert = (fahrenheit: number): number => {
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  console.log(`${fahrenheit}°F = ${celsius}°C`);
+  return fahrenheit;
+};
+
+fahrenheitConvert(140);
