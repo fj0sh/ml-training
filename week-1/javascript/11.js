39426d1130827e6f9a69ea242fd4d5d6e211e81e
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 15:19:26 +0800

    [JS-11] add stringConverter which converts an HTML entities to its corresponding character

--- week-1/javascript/11.js
+++ week-1/javascript/11.js
@@ -1,14 +1,26 @@
-/**
- * Write a function that converts HTML entities to their corresponding characters.
- * @param {string} str The string to convert.
- * @return {string} The string with HTML entities converted to their corresponding characters.
- */
+  /**
+   * Write a function that converts HTML entities to their corresponding characters.
+   * @param {string} str The string to convert.
+   * @return {string} The string with HTML entities converted to their corresponding characters.
+   */
 
-// ENTITY OBJECT
-const entities = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': "&quot;",
-  "'": "&apos;",
-};
+  // ENTITY OBJECT
+  const entities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+
+function stringConverter(str) {
+   let fixed = str;
+  
+   for (const entity in entities) {
+     fixed = fixed.split(entities[entity]).join(entity);
+   }
+  
+   return fixed;
+}
+
+  console.log(stringConverter("&amp; &lt; &gt; &quot; &apos;"))
