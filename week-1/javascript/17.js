bfc3a5b39d05cf30b9108dcfbc4858ce7f70da23
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 09:27:57 +0800

    [JS-17] Add password validator that validates the password with the given conditions

--- week-1/javascript/17.js
+++ week-1/javascript/17.js
@@ -19,3 +19,45 @@
  * @param {string} password
  * @returns {string}
  */
+
+const specChar = '!@#$%^&*()_+=<>/:"[]?'
+const charArr = specChar.split('')
+
+function passwordValidator(password){
+
+    if(password.length < 8){
+        return "Password must be at least 8 characters long"
+    }
+
+    hasUpper = false;
+    hasLower = false;
+    hasNum = false;
+    hasChar = false;
+
+    for(i of password){
+        if(i >= 'a' && i <= 'z'){
+            hasLower = true
+        }else if(i >= 'A' && i <= 'Z'){
+            hasUpper = true
+        }else if(i >= 0 && i <= 9){
+            hasNum = true
+        }else if(charArr.includes(i)){
+            hasChar = true
+        }
+    }
+
+    if(hasUpper && hasLower && hasNum && hasChar){
+        return 'Password is valid'
+    }
+
+    if(!hasLower){
+        return 'Password must contain at least one lowercase letter'
+    }else if(!hasUpper){
+        return 'Password must contain at least one uppercase letter'
+    }else if(!hasNum){
+        return 'Password must contain at least one number'
+    }else if(!hasChar){
+        return 'Password must contain at least one special character'
+    }
+}
+console.log(passwordValidator('?Password123'))
