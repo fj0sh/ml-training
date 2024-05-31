0a5b0b24e08d4f3ce6bd9f0f6be9dc4db9360085
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-31 13:34:36 +0800

    [JS-14] Add array checker that checks the lowest and the highest number in an array

--- week-1/javascript/14.js
+++ week-1/javascript/14.js
@@ -1,8 +1,18 @@
-/**
- * Write a function that accepts an array of numbers and displays the highest and lowest numbers in the array.
- * @param {number[]} arr The array of numbers.
- * @return {string} The highest and lowest numbers in the array.
- *
- * Example array: [5, 2, 8, 1, 9, 3]
- * Expected output: "The lowest number is 1 and the highest number is 9."
- */
+    /**
+     * Write a function that accepts an array of numbers and displays the highest and lowest numbers in the array.
+     * @param {number[]} arr The array of numbers.
+     * @return {string} The highest and lowest numbers in the array.
+     *
+     * Example array: [5, 2, 8, 1, 9, 3]
+     * Expected output: "The lowest number is 1 and the highest number is 9."
+     */
+
+    let arr = [5, 2, 8, 1, 9, 3]
+
+    const arrayCheck = () => {
+        const sorted = arr.sort()
+
+        console.log(`The lowest number is ${sorted[0]} and the highest number is ${sorted[arr.length-1]}.`)
+    }
+
+arrayCheck(arr);
