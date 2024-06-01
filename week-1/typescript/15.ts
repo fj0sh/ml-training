eb20a5ce1a2bc5207c0ef53fcde8dc7a071f6504
francis joshua cutamora <francisjoshuacutamora@gmail.com>
2024-06-01 23:02:16 +0800

    [TS-15] Add numiterator fn to show number wether its odd or even

--- week-1/typescript/15.ts
+++ week-1/typescript/15.ts
@@ -2,3 +2,15 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+function numIterator(n:number) {
+    for (let x = 1; x <= n; x++) {
+      if (x % 2 == 0) {
+        console.log("even");
+      } else {
+        console.log("odd");
+      }
+    }
+  }
+  
+  numIterator(20);
